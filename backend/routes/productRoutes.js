afa07c408c101180166ac6cb2f4e76d837a09954
LAPTOP-SNSEHSAR\ayush <ayushjain2205@gmail.com>
2020-12-04 23:58:15 +0530

    routes reconfigured

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,7 +6,7 @@ router.get("/", (req, res) => {
   res.json(products);
 });
 
-router.get("/id", (req, res) => {
+router.get("/:id", (req, res) => {
   const product = products.find((p) => p._id === req.params.id);
   res.json(product);
 });
