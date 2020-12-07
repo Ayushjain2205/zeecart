98bcc04d5ca60aea2a8980a512beecae1f19038b
LAPTOP-SNSEHSAR\ayush <ayushjain2205@gmail.com>
2020-12-08 00:12:09 +0530

    final changes made

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,33 +1,23 @@
-import express from "express";
-import asyncHandler from "express-async-handler";
-const router = express.Router();
-import Product from "../models/productModel.js";
+import express from 'express'
+const router = express.Router()
+import {
+  getProducts,
+  getProductById,
+  deleteProduct,
+  createProduct,
+  updateProduct,
+  createProductReview,
+  getTopProducts,
+} from '../controllers/productController.js'
+import { protect, admin } from '../middleware/authMiddleware.js'
 
-// @desc Fetch all products
-//@route GET /api/products
-//@access Public
-router.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
-  })
-);
+router.route('/').get(getProducts).post(protect, admin, createProduct)
+router.route('/:id/reviews').post(protect, createProductReview)
+router.get('/top', getTopProducts)
+router
+  .route('/:id')
+  .get(getProductById)
+  .delete(protect, admin, deleteProduct)
+  .put(protect, admin, updateProduct)
 
-// @desc Fetch a single product
-//@route GET /api/products/:id
-//@access Public
-router.get(
-  "/:id",
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404);
-      throw new Error("Product not found");
-    }
-  })
-);
-
-export default router;
+export default router
